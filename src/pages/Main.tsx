4fddce8aef03257241d4050a0d37608658dc31a4
moon <moonjh9392@gmail.com>
2023-01-02 17:58:54 +0900

    client

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,35 +1,90 @@
-import { useSelector, useDispatch } from 'react-redux';
-
-import { down, init, up, selectValue } from '../slices/counterSlice';
 import styled from 'styled-components';
+import TodoList from '../components/TodoList';
+import { ChangeEvent, useEffect, useState } from 'react';
+import { getData, postData } from '../api/api';
 
-const MainStyle = styled.div`
-	margin-top: 300px;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-`;
+export interface Todo {
+	id?: string;
+	title: string;
+	content: string;
+	createdAt?: string;
+	updatedAt?: string;
+}
 
 const Main = () => {
-	const dispach = useDispatch();
-	const count = useSelector(selectValue);
-	const addNumber = () => {
-		dispach(up(1));
+	const [todoList, setTodoList] = useState<Array<Todo>>([]);
+	const [todo, setTodo] = useState<Todo>({ title: '', content: '' });
+
+	const onChange = (e: ChangeEvent<HTMLInputElement>): void => {
+		const { name, value } = e.target;
+		//name과 value를 뽑아 객체에 덮어씌운다.
+		setTodo({
+			...todo,
+			[name]: value,
+		});
 	};
-	const minusNumber = () => {
-		dispach(down(1));
-	};
-	const initNumber = () => {
-		dispach(init());
+	useEffect(() => {
+		getData('http://localhost:8080/todos/').then((res) =>
+			setTodoList(res.data),
+		);
+	}, []);
+
+	/**
+	 * todo 생성
+	 */
+	const createTodo = (): void => {
+		postData('http://localhost:8080/todos/', todo);
 	};
 	return (
 		<MainStyle>
-			<div>{count}</div>
-			<button onClick={addNumber}>+</button>
-			<button onClick={minusNumber}>-</button>
-			<button onClick={initNumber}>초기화</button>
+			<div className="background">
+				<div className="menu">
+					<form>
+						<input
+							type="text"
+							name="title"
+							onChange={onChange}
+							autoComplete="off"
+							placeholder="title"
+						/>
+						<input
+							type="text"
+							name="content"
+							onChange={onChange}
+							autoComplete="off"
+							placeholder="content"
+						/>
+						<button onClick={createTodo}>추가</button>
+					</form>
+
+					<TodoList todoList={todoList} />
+				</div>
+			</div>
 		</MainStyle>
 	);
 };
 
 export default Main;
+
+const MainStyle = styled.div`
+	.menu {
+		position: relative;
+		margin: 0 auto;
+		max-width: 718px;
+
+		@media screen and (max-width: 768px) {
+			padding: 128px 24px;
+			top: -128px;
+		}
+	}
+
+	.background {
+		background-color: whitesmoke;
+		padding-bottom: 80px;
+		padding-top: 40px;
+		height: 100vh;
+		@media screen and (min-height: 1200px) {
+			height: 100%;
+		}
+	}
+`;
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
 import Loading from '../components/common/Loading';
 
+const Login = lazy(() => import('../pages/Login'));
 const Main = lazy(() => import('../pages/Main'));
 
 const AppRouter = () => {
@@ -9,7 +10,8 @@ const AppRouter = () => {
 		<BrowserRouter>
 			<Suspense fallback={<Loading />}>
 				<Routes>
-					<Route path="/" element={<Main />} />
+					<Route path="/" element={<Login />} />
+					<Route path="/main" element={<Main />} />
 					<Route path="*" element={<Navigate to="/" replace />} />
 				</Routes>
 			</Suspense>
